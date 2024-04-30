d05e094e13070d82d0dd0d1b8ca6ae0e0d37357d
Ruby Lee <rubylee@MacBook-Pro-2.local>
2024-04-30 01:23:40 -0700

    Update some tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { copy, menuData } from './DefaultData/pageData';
 
-test('renders learn react link', () => {
+test('renders App', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const title = screen.getByText(copy.title);
+  expect(title).toBeInTheDocument();
+
+
 });
+
+test('render top nav', () => {
+  render(<App />);
+
+  let topNavTitles = menuData.map((menu) => {
+    return menu.name
+  })
+
+  const title = screen.getByText(topNavTitles[0]);
+  expect(title).toBeInTheDocument();
+})
